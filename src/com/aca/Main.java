bc67ff09d8b2c3124914ce5861a029dbff6e5558
Varuzhan1996 <varuzhanyeranosyan1996@mail.ru>
2022-01-12 21:12:00 +0400

    First commit

---
+++ src/com/aca/Main.java
@@ -0,0 +1,22 @@
+package com.aca;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Task 1
+//       /* System.out.print("Plese Input Number : ");
+//        int n = new Scanner(System.in).nextInt();
+//        int sum = 0;
+//        for (int i = 1; i <= n; i++) {
+//            sum += i;
+//        }
+//        System.out.println("The Output is : " + sum);*/
+
+    }
+}
+
+
+
+
+
---
+++ src/com/aca/Recursion.java
@@ -0,0 +1,32 @@
+package com.aca;
+
+public class Recursion {
+    public static void MainFunc() {
+        /*System.out.println(fib(5));*/
+//        System.out.println(numDigit("mhglj4655mgchfgh"));
+
+    }
+
+    /*public static int numDigit(String str,int n) {
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i)>='0' && str.charAt(i) <='9'){
+                sum++;
+            }
+        }
+        return sum;
+    }*/
+    /*public static int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }*/
+
+
+
+
+}
+
+
+
