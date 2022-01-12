8d66913c995f2a29e2112d80101aa40306ee39e2
Varuzhan1996 <varuzhanyeranosyan1996@mail.ru>
2022-01-12 21:16:47 +0400

    Commit

--- src/com/aca/Main.java
+++ src/com/aca/Main.java
@@ -5,14 +5,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //        Task 1
-//       /* System.out.print("Plese Input Number : ");
-//        int n = new Scanner(System.in).nextInt();
-//        int sum = 0;
-//        for (int i = 1; i <= n; i++) {
-//            sum += i;
-//        }
-//        System.out.println("The Output is : " + sum);*/
+       /* System.out.print("Plese Input Number : ");
+        int n = new Scanner(System.in).nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("The Output is : " + sum);*/
+        System.out.print("Plese Input String : ");
+        String str = new Scanner(System.in).nextLine();
+        System.out.println(iSpalindrome(str));
+    }
 
+    static boolean iSpalindrome(String str) {
+        int length = str.length();
+        int start = 0;
+        int end = length - 1;
+        while (end > start) {
+            char forwardChar = str.charAt(start++);
+            char backwardChar = str.charAt(end--);
+            if (forwardChar != backwardChar) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
